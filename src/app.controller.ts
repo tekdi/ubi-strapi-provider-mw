cdbb13fc7bab77302229a42b1917b9aef40cb7b2
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-04-30 19:24:19 +0530

    feat: add Swagger support and update dependencies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('App') // Grouping the APIs under the "App" tag in Swagger
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get Hello Message', description: 'Returns a hello message from the AppService.' })
   getHello(): string {
     return this.appService.getHello();
   }
