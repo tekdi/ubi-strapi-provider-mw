456664164d9ff6ef19917c4d1a799c05c175aa9a
GitVivekHub <vivek.bhandurge@tekditechnologies.com>
2025-05-07 19:02:22 +0530

    1.Search financial support mapping for strapi,2.Select financial support mapping for strapi

--- src/benefits/benefits.controller.ts
+++ src/benefits/benefits.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, UseFilters } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SearchRequestDto } from './dto/search-request.dto';
 import { BenefitsService } from './benefits.service';
@@ -7,28 +16,28 @@ import { AllExceptionsFilter } from 'src/common/filters/exception.filters';
 @ApiTags('Benefits') // Grouping the APIs under the "Benefits" tag in Swagger
 @Controller('benefits')
 export class BenefitsController {
+  constructor(private readonly benefitsService: BenefitsService) {}
 
-    constructor(private readonly benefitsService: BenefitsService) { }
+  @UseFilters(new AllExceptionsFilter())
+  @ApiOperation({
+    summary: 'Get Benefits by ID',
+    description: 'Fetch benefits by their unique identifier.',
+  })
+  @Get('select/:id')
+  @HttpCode(HttpStatus.OK)
+  getBenefitsById(@Param('id') id: string): any {
+    return this.benefitsService.getBenefitsById(id);
+  }
 
-    @UseFilters(new AllExceptionsFilter())
-    @ApiOperation({
-        summary: 'Get Benefits by ID',
-        description: 'Fetch benefits by their unique identifier.',
-    })
-    @Get('getById/:docid')
-    @HttpCode(HttpStatus.OK)
-    getBenefitsById(@Param('docid') id: string): any {
-        return this.benefitsService.getBenefitsById(id);
-    }
-
-    @UseFilters(new AllExceptionsFilter())
-    @Post('search')
-    @HttpCode(HttpStatus.OK)
-    @ApiOperation({
-        summary: 'Search Benefits',
-        description: 'Search for benefits based on the provided context and message.',
-    })
-    searchBenefits(@Body() searchRequestDto: SearchRequestDto): any {
-        return this.benefitsService.searchBenefits(searchRequestDto);
-    }
+  @UseFilters(new AllExceptionsFilter())
+  @Post('search')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Search Benefits',
+    description:
+      'Search for benefits based on the provided context and message.',
+  })
+  searchBenefits(@Body() searchRequestDto: SearchRequestDto): any {
+    return this.benefitsService.searchBenefits(searchRequestDto);
+  }
 }
