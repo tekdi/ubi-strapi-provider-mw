3d6405283f52f441769e46fea993d29d0fe4e2ba
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2025-05-07 13:22:08 +0530

    feat: rename ApplicationVCs to ApplicationFiles; update related migrations and schema

--- prisma/migrations/20250506083341_init/migration.sql
+++ prisma/migrations/20250506083341_init/migration.sql
@@ -8,16 +8,16 @@ CREATE TABLE "Applications" (
 );
 
 -- CreateTable
-CREATE TABLE "ApplicationVCs" (
+CREATE TABLE "ApplicationFiles" (
     "id" SERIAL NOT NULL,
     "applicationFilesId" TEXT NOT NULL,
     "applicationId" INTEGER NOT NULL,
 
-    CONSTRAINT "ApplicationVCs_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "ApplicationFiles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Applications_benefitId_key" ON "Applications"("benefitId");
 
 -- AddForeignKey
-ALTER TABLE "ApplicationVCs" ADD CONSTRAINT "ApplicationVCs_applicationId_fkey" FOREIGN KEY ("applicationId") REFERENCES "Applications"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "ApplicationFiles" ADD CONSTRAINT "ApplicationFiles_applicationId_fkey" FOREIGN KEY ("applicationId") REFERENCES "Applications"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
