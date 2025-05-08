e735aa5e22ec7cbeceb737024df693ae4b245dc5
Manoj L <manojLondhe@users.noreply.github.com>
2025-05-08 19:41:16 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable validation
-  app.useGlobalPipes(new ValidationPipe({
+  /*app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
     forbidNonWhitelisted: true,
-  }));
+  }));*/
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
