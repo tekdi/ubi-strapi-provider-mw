322f9c1d211e014659bb14e8b2cc42c28e301674
ankush_m <ankush_m@techjoomla.com>
2025-05-08 19:14:58 +0530

    Provider - BE - Provider User Login with Email + Pwd

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Body,
+  Req,
+} from '@nestjs/common';
+import { LoginDto } from './dto/login-request.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(public authService: AuthService) {}
+
+  @Post('/login')
+  @UsePipes(ValidationPipe)
+  login(@Body() req: LoginDto) {
+    return this.authService.login(req);
+  }
+}
---
+++ src/common/middlewares/auth.middleware.ts
@@ -0,0 +1,86 @@
+// src/middleware/auth.middleware.ts
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { jwtDecode } from 'jwt-decode';
+import { PrismaService } from '../../prisma.service';
+
+// Extend the Request interface to include mw_userid
+declare module 'express-serve-static-core' {
+  interface Request {
+    mw_userid?: number | string;
+  }
+}
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private prisma: PrismaService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new UnauthorizedException(
+        'Missing or malformed Authorization header',
+      );
+    }
+
+    if (req.headers.authorization) {
+      let bearerToken: string = '';
+      let bearerTokenParts: string[] = [];
+
+      // Get userid from  auth/login jwt token
+      const authToken = req?.headers?.authorization;
+      const authTokenTemp = req?.headers?.authorization.split(' ');
+
+      // If Bearer word not found in auth header value
+      if (authTokenTemp[0] !== 'Bearer') {
+        req.mw_userid = '';
+      }
+      // Get trimmed Bearer token value by skipping Bearer value
+      else {
+        bearerToken = authHeader.split(' ')[1];
+      }
+
+      // If Bearer token value is not passed
+      if (!bearerToken) {
+        req.mw_userid = '';
+      }
+      // Lets split token by dot (.)
+      else {
+        bearerTokenParts = bearerToken.split('.');
+      }
+
+      // Since JWT has three parts - seperated by dots(.), lets split token
+      if (bearerTokenParts.length < 3) {
+        req.mw_userid = '';
+      }
+
+      try {
+        const decoded: any = jwtDecode(authToken);
+        let userId: string = String(decoded.id);
+
+        console.log('userId---->>>>', userId);
+
+        // Get the middleware user id from the strapi user id
+        let user = await this.prisma.users.findUnique({
+          where: { s_id: userId },
+        });
+
+        if (!user) {
+          throw new UnauthorizedException('User not found');
+        }
+
+        req.mw_userid = user.id;
+      } catch (error) {
+        console.error('Something went wrong:', error);
+        req.mw_userid = '';
+      }
+    }
+
+    next();
+  }
+}
