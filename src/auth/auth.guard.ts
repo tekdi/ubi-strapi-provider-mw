712c41ad67ba93dc7d16896ff91f27e1e014daa0
unknown <milind.sansare@tekditechnologies.com>
2025-05-09 16:58:26 +0530

    enable public route in auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,12 +7,25 @@ import {
 import { Request } from 'express';
 import { jwtDecode } from 'jwt-decode';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const ctx = context.switchToHttp();
     const request = ctx.getRequest<Request>();
 
