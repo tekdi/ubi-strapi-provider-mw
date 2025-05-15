4d6a9336746ef5b8c3dd436817182ac92708af30
milindSansare <milind.sansare@tekditechnologies.com>
2025-05-15 16:23:10 +0530

    Csv export (#39)
    * applications list and status update apis
    
    * removed application status enum
    
    * removed enum status from db
    
    * quick fix in controller
    
    * make benefitid mandatory
    
    * make benefitid mandatory
    
    * revert migrations file
    
    * apply auth guard to protected routes
    
    * enable public route in auth guard
    
    * added authguard by using useAuthGuard and removed previously added authguard
    
    * removed public route
    
    * added auth in benefits and removed for create application route
    
    * updated swagger documentation
    
    * added benefits details in application list and details apis
    
    * remov interface
    
    * changed api response of list applications and application details
    
    * resolved conflicts
    
    * resolved conflicts
    
    * change benefitsDetails key to benefit array
    
    * change application list res
    
    * rename as title
    
    * created csv export api
    
    * created csv export api
    
    * changes as mentioned in comments
    
    * add finalamount in amount column

---
+++ src/applications/dto/csvexport-applications.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CsvExportApplicationsDto {
+  @ApiProperty({
+    description: 'Benefit ID to filter applications',
+    required: true,
+    example: 'benefit-123'
+  })
+  @IsString()
+  @IsNotEmpty()
+  benefitId: string;
+
+  @ApiProperty({
+    description: 'Type of report (as per reports.json)',
+    required: true,
+    example: 'format1'
+  })
+  @IsString()
+  @IsNotEmpty()
+  type: string;
+} 
