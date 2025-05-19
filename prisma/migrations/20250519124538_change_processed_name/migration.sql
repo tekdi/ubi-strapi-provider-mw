1b9e3a39f660c586e0b727427d2894b612ba6dc2
Manoj L <manojLondhe@users.noreply.github.com>
2025-05-19 20:24:28 +0530

    Delete prisma/migrations/20250519124538_change_processed_name/migration.sql

--- prisma/migrations/20250519124538_change_processed_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `processedAt` on the `Applications` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Applications" DROP COLUMN "processedAt",
-ADD COLUMN     "calculationsProcessedAt" TIMESTAMP(3);
