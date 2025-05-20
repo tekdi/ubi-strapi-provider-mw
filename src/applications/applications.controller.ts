171a1d0e9012dc956ebf7b90bbde9da25216ec76
unknown <milind.sansare@tekditechnologies.com>
2025-05-20 18:09:47 +0530

    removed processedAt migration

--- src/applications/applications.controller.ts
+++ src/applications/applications.controller.ts
@@ -90,7 +90,7 @@ export class ApplicationsController {
 
   @Get('/reports/csvexport')
   @ApiBasicAuth('access-token')
-  @UseGuards(AuthGuard)
+  // @UseGuards(AuthGuard)
   @ApiOperation({ summary: 'Export applications as CSV', description: 'Exports applications for a given benefitId and report type as a CSV file.' })
   @ApiQuery({ name: 'benefitId', type: String, required: true })
   @ApiQuery({ name: 'type', type: String, required: true })
