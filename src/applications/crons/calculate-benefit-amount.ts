a9451c31c4fb8875a19f17102f39abffee69c1f7
unknown <milind.sansare@tekditechnologies.com>
2025-05-21 17:20:31 +0530

    removed where in cron

--- src/applications/crons/calculate-benefit-amount.ts
+++ src/applications/crons/calculate-benefit-amount.ts
@@ -6,53 +6,61 @@ import { ApplicationsService } from '../applications.service';
 import { CronJob } from 'cron';
 @Injectable()
 export class ApplicationStatusUpdate {
-    constructor(
-        private readonly prisma: PrismaService,
-        private readonly configService: ConfigService,
-        private readonly applicationsService: ApplicationsService,
-        private readonly schedulerRegistry: SchedulerRegistry,
-    ) { }
-    onModuleInit() {
-        const cronExpression = this.configService.get('BENEFIT_CALCULATIONS_CRON_TIME') ?? '*/30 * * * *';
-        const job = new CronJob(cronExpression, () => this.updateApplicationStatusCron());
+	constructor(
+		private readonly prisma: PrismaService,
+		private readonly configService: ConfigService,
+		private readonly applicationsService: ApplicationsService,
+		private readonly schedulerRegistry: SchedulerRegistry
+	) {}
+	onModuleInit() {
+		const cronExpression =
+			this.configService.get('BENEFIT_CALCULATIONS_CRON_TIME') ??
+			'*/30 * * * *';
+		const job = new CronJob(cronExpression, () =>
+			this.updateApplicationStatusCron()
+		);
 
-        this.schedulerRegistry.addCronJob('application-status-update', job);
-        job.start();
-    }
-    private async updateApplicationStatusCron() {
-        try {
-            const applications = await this.getApplications();
-            await this.processApplications(applications);
-        } catch (error) {
-            Logger.error(`Error in 'benefit calculation cron': ${error.message}`, error.stack);
-        }
-    }
+		this.schedulerRegistry.addCronJob('application-status-update', job);
+		job.start();
+	}
+	private async updateApplicationStatusCron() {
+		try {
+			const applications = await this.getApplications();
+			await this.processApplications(applications);
+		} catch (error) {
+			Logger.error(
+				`Error in 'benefit calculation cron': ${error.message}`,
+				error.stack
+			);
+		}
+	}
 
-    async getApplications() {
-        let dt = new Date();
-        let BENEFITS_CALCULATION_LAST_PROCESS_HOURS = Number(
-            this.configService.get('BENEFIT_CALCULATIONS_LAST_PROCESS_HOURS') ?? 24,
-        );
-        let filterTimestamp = new Date(
-            dt.setHours(dt.getHours() - BENEFITS_CALCULATION_LAST_PROCESS_HOURS),
-        ).toISOString();
-        return this.prisma.$queryRaw<
-            Array<{ id: number; }>
-        >`
+	async getApplications() {
+		const dt = new Date();
+		const BENEFITS_CALCULATION_LAST_PROCESS_HOURS = Number(
+			this.configService.get('BENEFIT_CALCULATIONS_LAST_PROCESS_HOURS') ??
+				24
+		);
+		const filterTimestamp = new Date(
+			dt.setHours(dt.getHours() - BENEFITS_CALCULATION_LAST_PROCESS_HOURS)
+		).toISOString();
+		return this.prisma.$queryRaw<Array<{ id: number }>>`
             SELECT id
             FROM "Applications"
             WHERE "calculatedAmount" IS NULL
             AND ("calculationsProcessedAt" IS NULL OR "calculationsProcessedAt" <= ${filterTimestamp}::timestamp)
             LIMIT ${this.configService.get('BENEFIT_CALCULATIONS_BATCH_SIZE') ?? 10}::bigint OFFSET 0
             `;
-    }
-    private async processApplications(applications: any[]) {
-        for (const app of applications) {
-            try {
-                const response = await this.applicationsService.calculateBenefit(Number(app.id))
-            } catch (err) {
-                Logger.warn(`Failed to update application ${app.id}: ${err.message}`);
-            }
-        }
-    }
+	}
+	private async processApplications(applications: any[]) {
+		for (const app of applications) {
+			try {
+				await this.applicationsService.calculateBenefit(Number(app.id));
+			} catch (err) {
+				Logger.warn(
+					`Failed to update application ${app.id}: ${err.message}`
+				);
+			}
+		}
+	}
 }
