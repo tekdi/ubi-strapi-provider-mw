6cf804a02057d09041c2128f04892a2dce4227c4
Manoj <manoj_l@techjoomla.com>
2025-06-12 15:17:30 +0530

    Issue #000 feat: Update pre-commit hook to apply ESLint for staged files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,11 +5,14 @@ import { AppService } from './app.service';
 @ApiTags('App') // Grouping the APIs under the "App" tag in Swagger
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @ApiOperation({ summary: 'Get Hello Message', description: 'Returns a hello message from the AppService.' })
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	@ApiOperation({
+		summary: 'Get Hello Message',
+		description: 'Returns a hello message from the AppService',
+	})
+	getHello(): string {
+		return this.appService.getHello();
+	}
 }
